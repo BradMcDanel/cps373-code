866bf386ea18569ad9eb4fa878ef9e96a9820455
Brad McDanel <bmcdanel@fandm.edu>
2022-02-02 13:44:54 -0500

    React code

---
+++ 05/shop/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+{/*import Items from './Items';*/}
+
+function App() {
+ return (
+   <div className="App">
+     <h2>Items</h2>
+     {/*<Items/>*/}
+   </div>
+ );
+}
+export default App;
